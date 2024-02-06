58ef723403f9291fef5921439ce2f7c8499a1012
morry <morry@192.168.1.6>
2024-02-06 11:39:36 +0900

    DBUtil作成

---
+++ src/main/java/utils/DBUtil.java
@@ -0,0 +1,27 @@
+package utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import constants.JspConst;
+
+public class DBUtil {
+
+	private static EntityManagerFactory emf;
+
+	//EntityManagerインスタンスを生成
+	public static EntityManager createEntityManager() {
+		return _getEntityManagerFactory().createEntityManager();
+	}
+
+	//EntityManagerFactoryインスタンスを生成
+	private static EntityManagerFactory _getEntityManagerFactory() {
+		if(emf == null) {
+			emf = Persistence.createEntityManagerFactory(JspConst.PERSISTENCE_UNIT_NAME);
+
+		}
+		return emf;
+	}
+
+}
