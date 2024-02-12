93daae0231409b25c2eb31978fe8eb494f306388
morry <morry@192.168.1.6>
2024-02-12 13:40:42 +0900

    ReportService,ReportAction　修正

--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -53,7 +53,7 @@ public class ReportService extends ServiceBase{
 	 *@return 一覧画面に表示するデータのリスト
 	 */
 	public List<ReportView>getAllPerPage(int page){
-		List<Report>reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL_MINE,Report.class)
+		List<Report>reports = em.createNamedQuery(JpaConst.Q_REP_GET_ALL,Report.class)
 				.setFirstResult(JpaConst.ROW_PER_PAGE * (page-1))
 				.setMaxResults(JpaConst.ROW_PER_PAGE)
 				.getResultList();
